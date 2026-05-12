d8074ea2bf66b4858679a5f081cd9f5452d61318
nomikonnn <mallman0512@gmail.com>
2026-05-12 19:42:00 +0400

    Change default DB password in index.js
    Updated default database password for Sequelize connection.

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'railway',
   process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_PASSWORD || 'UTRSwBcrLatMUSaornevXBNjsWjVvfbF',
   {
     host: process.env.DB_HOST || 'mysql.railway.internal',
     port: process.env.DB_PORT || 3306,
