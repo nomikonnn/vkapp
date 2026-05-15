fb0b7ee8ef2945903255124b6e8e15cc72edeb27
Vadim <mallman0512@gmail.com>
2026-05-16 01:13:46 +0400

    Fix categories.routes

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-const Category = require('../models/Category');
+const { Category } = require('../models'); // ВАЖНО: деструктуризация!
 const authenticateToken = require('../middleware/authenticateToken');
 const checkAdmin = require('../middleware/checkAdmin');
 
