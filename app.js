ff47bac1c5d481518aabbb95f5a4cd32ceafcb89
Vadim <mallman0512@gmail.com>
2026-05-16 01:54:33 +0400

    Fix app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // Только одно объявление!
 const helmet = require('helmet');
 const { errorHandler } = require('./middleware/errorHandler');
 const routes = require('./routes');
 
-const app = express();
+const app = express(); // Объявляем ДО использования!
 
-// Разрешаем запросы с любого origin (для учебного проекта)
+// CORS - только ОДНА настройка!
 app.use(cors({
-  origin: '*',
+  origin: [
+    'https://vk.com',
+    'https://prod-app54587418-e3807501de96.pages-ac.vk-apps.com',
+    'http://localhost:5173',
+    'http://localhost:5174' // На случай если порт занят
+  ],
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
